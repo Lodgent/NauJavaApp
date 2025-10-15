811c585eef507f02f0cda38048fc3c93469d8868
Гришмановский Владимир Андреевич <vovagri2003@gmail.com>
2025-10-15 22:20:28 +0500

    fix app properties and add ConsoleApp class

---
+++ src/main/java/ru/Vladimir/NauJava/ConsoleApp.java
@@ -0,0 +1,23 @@
+package ru.Vladimir.NauJava;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Scanner;
+
+public class ConsoleApp {
+    @Autowired
+    private FileEntity fileService;
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("1. Загрузить файл");
+            System.out.println("2. Сгенерировать ссылку");
+            System.out.println("3. Скачать файл");
+            System.out.println("4. Выйти");
+
+            int choice = scanner.nextInt();
+            // обработка выбора
+        }
+    }
+}
