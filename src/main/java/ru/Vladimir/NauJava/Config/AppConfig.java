3f0b76d82ec389d8a01b732016494fb169ced3ae
Гришмановский Владимир Андреевич <vovagri2003@gmail.com>
2025-10-22 22:33:21 +0500

    lab4(step3) fix AppConfig, add service

--- src/main/java/ru/Vladimir/NauJava/Config/AppConfig.java
+++ src/main/java/ru/Vladimir/NauJava/Config/AppConfig.java
@@ -11,9 +11,29 @@ import ru.Vladimir.NauJava.Models.FileEntity;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Configuration;
+
+
+
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
 
+    @ComponentScan(basePackages = "ru.Vladimir.NauJava")
+    @EnableJpaRepositories(basePackages = "ru.Vladimir.NauJava.repository")
+    public class RepositoryConfig {
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
 
     @Bean
     @Scope(BeanDefinition.SCOPE_SINGLETON)
